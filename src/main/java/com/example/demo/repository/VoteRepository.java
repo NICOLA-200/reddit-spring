f2dcc4878856224e95078f7d769d0c33be579218
NICOLA-200 <fatepepe66@gmail.com>
2024-07-19 20:52:13 +0200

    repository added

---
+++ src/main/java/com/example/demo/repository/VoteRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.model.Vote;
+import com.example.demo.model.User;
+import com.example.demo.model.Post;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
+}
